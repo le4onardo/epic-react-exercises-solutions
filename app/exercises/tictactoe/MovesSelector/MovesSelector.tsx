14e55b0f8c59fd47093637bab6b1f939e21e77bf
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2023-05-14 04:08:06 +0000

    MovesSelector component

---
+++ app/exercises/tictactoe/MovesSelector/MovesSelector.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function MovesSelector ({
+    movesCount,
+    onMoveSelected,
+    currentMove
+}:{
+    movesCount: number,
+    onMoveSelected: (index: number)=>void,
+    currentMove: number
+}) {
+    const moves=[];
+    for (let i = 0; i<=movesCount; i++) {
+        moves.push(
+        <button
+            key={i}
+            onClick={() => {
+                onMoveSelected(i)
+            }}
+            disabled={i === currentMove}
+        >
+            {i}
+        </button>)
+    }
+    return <> {moves} </>;
+}
